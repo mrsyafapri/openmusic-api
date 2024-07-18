ec9853c59bbafeb581d236cb6c26e4421293bf8b
mrsyafapri <rizkisyafapri@gmail.com>
2024-07-18 11:31:39 +0700

    fix: eslint untuk versi 1

--- src/api/songs/handler.js
+++ src/api/songs/handler.js
@@ -10,8 +10,9 @@ class SongsHandler {
 
   async postSongHandler(request, h) {
     this._validator.validateSongPayload(request.payload);
-    const { title, year, genre, performer, duration, albumId } =
-      request.payload;
+    const {
+      title, year, genre, performer, duration, albumId,
+    } = request.payload;
 
     const songId = await this._service.addSong({
       title,
--- src/services/postgres/SongsService.js
+++ src/services/postgres/SongsService.js
@@ -9,7 +9,9 @@ class SongsService {
     this._pool = new Pool();
   }
 
-  async addSong({ title, year, genre, performer, duration, albumId }) {
+  async addSong({
+    title, year, genre, performer, duration, albumId,
+  }) {
     const id = `song-${nanoid(16)}`;
 
     const query = {
@@ -63,7 +65,9 @@ class SongsService {
     return result.rows.map(mapDBToModel)[0];
   }
 
-  async editSongById(id, { title, year, genre, performer, duration, albumId }) {
+  async editSongById(id, {
+    title, year, genre, performer, duration, albumId,
+  }) {
     const query = {
       text: "UPDATE songs SET title = $1, year = $2, genre = $3, performer = $4, duration = $5, album_id = $6 WHERE id = $7 RETURNING id",
       values: [title, year, genre, performer, duration, albumId, id],
